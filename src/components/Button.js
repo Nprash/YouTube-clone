bb4e7ef5390fb135138b7702198542885faf34de
N Prasanth <nimmanaprasanthlove@gmail.com>
2024-01-06 12:38:50 +0530

    buttonlist font updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import React from "react"
 const Button = ({name}) =>{
     return (
         <>
-            <button className={`px-2 m-2  bg-gray-200 rounded-lg whitespace-nowrap `}>{name}</button>
+            <button className={`md:px-2 md:m-2 text-[12px] md:text-sm lg:text-md xl:text-lg bg-gray-200 rounded-lg whitespace-nowrap `}>{name}</button>
         </>
     )
 }
